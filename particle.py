f7675f84719468dc8773d481739c4b97fcca0c11
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-03-13 20:44:50 +0100

    Added best position and fitness for particles & isolated problem initialisation

--- particle.py
+++ particle.py
@@ -1,9 +1,12 @@
 import numpy as np
+import math
 
 class Particle:
 	def __init__(self):
 		self.position = np.random.randint(1000, size=(3, 1))
 		self.velocity = np.random.randint(-50, 50, size=(3, 1))
+		self.best_position = self.position
+		self.best_fitness = math.inf
 	
 	def get_distance_from_objective(self, objective):
 		return np.linalg.norm(np.subtract(self.position, objective))
--- pso.py
+++ pso.py
@@ -3,14 +3,17 @@ from particle import *
 import numpy as np
 
 
+def initialize_problem():
+	swarm = generate_random_population()
+	objective = generate_random_objective()
+	return swarm, objective
+
+def generate_random_population():
+	swarm = Swarm(100)
+	return swarm
+
 def generate_random_objective():
 	return np.random.randint(1000, size=(3, 1))
 
 
-objective = generate_random_objective()
-print(objective)
-
-particle = Particle()
-particle.print()
-
-print(particle.get_distance_from_objective(objective))
+swarm, objective = initialize_problem()
