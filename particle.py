848244d9fc0169cf49958428b4daf3b848186b48
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-03-13 20:35:32 +0100

    Added particle update position method and in bounds particle position checking

--- particle.py
+++ particle.py
@@ -8,5 +8,23 @@ class Particle:
 	def get_distance_from_objective(self, objective):
 		return np.linalg.norm(np.subtract(self.position, objective))
 	
+	def update_position(self):
+		self.position = np.round(np.add(self.position, self.velocity))
+		self.keep_inbounds()
+	
+	def keep_inbounds(self):
+		if self.position[0]>1000:
+			self.position[0] = 1000
+		if self.position[0]<0:
+			self.position[0] = 0
+		if self.position[1]>1000:
+			self.position[1] = 1000
+		if self.position[1]<0:
+			self.position[1] = 0
+		if self.position[2]>1000:
+			self.position[2] = 1000
+		if self.position[2]<0:
+			self.position[2] = 0
+	
 	def print(self):
 		print(self.position, self.velocity)
