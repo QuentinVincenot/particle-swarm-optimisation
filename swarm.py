a21fceef8a14dedcc09b030015c069d95b2b86bb
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-03-13 20:41:02 +0100

    Implemented particles position updating for all particles in a swarm

--- swarm.py
+++ swarm.py
@@ -6,6 +6,12 @@ class Swarm:
 		for i in range(pop_length):
 			self.particles += [Particle()]
 
+	def update_particles_positions(self):
+		number_of_particles = len(self.particles)
+		for i in range(number_of_particles):
+			current_particle = self.particles[i]
+			current_particle.update_position();
+
 	def print(self):
 		for i in range(len(self.particles)):
 			self.particles[i].print()
