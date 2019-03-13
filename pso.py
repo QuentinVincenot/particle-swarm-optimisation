615517ff61165028e55a85d1aa5d1f2d5899e400
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-03-13 21:00:34 +0100

    Deleted not used anymore libraries in main program

--- pso.py
+++ pso.py
@@ -1,9 +1,6 @@
 from swarm import *
 from visualisation import *
 
-import matplotlib.pyplot as plt
-import mpl_toolkits.mplot3d.axes3d as p3
-import matplotlib.animation as animation
 import numpy as np
 
 
