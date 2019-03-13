3901966f42514735cada41ca11cf132d412c01f9
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-03-13 20:49:23 +0100

    Developed velocities and best finess updating for particles and swarm

--- particle.py
+++ particle.py
@@ -11,6 +11,23 @@ class Particle:
 	def get_distance_from_objective(self, objective):
 		return np.linalg.norm(np.subtract(self.position, objective))
 	
+	def update_best_fitness(self, objective):
+		current_fitness = self.get_distance_from_objective(objective)
+		if current_fitness < self.best_fitness:
+			self.best_position = self.position
+			self.best_fitness = current_fitness
+
+	def update_velocity(self, swarm_best_position):
+		velocity_factor = 0.4
+		cognitive_intelligence = 0.6
+		collective_intelligence = 0.8
+		random_factor_1 = 2 * np.random.random_sample() - 1
+		random_factor_2 = 2 * np.random.random_sample() - 1
+		new_velocity = velocity_factor * self.velocity
+		new_velocity += cognitive_intelligence * random_factor_1 * (np.subtract(self.best_position, self.position))
+		new_velocity += collective_intelligence * random_factor_2 * (np.subtract(swarm_best_position, self.position))
+		self.velocity = new_velocity
+
 	def update_position(self):
 		self.position = np.round(np.add(self.position, self.velocity))
 		self.keep_inbounds()
--- swarm.py
+++ swarm.py
@@ -5,6 +5,33 @@ class Swarm:
 		self.particles = []
 		for i in range(pop_length):
 			self.particles += [Particle()]
+		self.best_position = np.random.randint(1000, size=(3, 1))
+		self.best_fitness = math.inf
+	
+	def update_particles_best_fitness(self, objective):
+		number_of_particles = len(self.particles)
+		for i in range(number_of_particles):
+			current_particle = self.particles[i]
+			current_particle.update_best_fitness(objective)
+
+	def update_swarm_best_fitness(self, objective):
+		number_of_particles = len(self.particles)
+		temporary_best_position = np.random.randint(1000, size=(3, 1))
+		temporary_best_fitness = math.inf
+		for i in range(number_of_particles):
+			current_particle = self.particles[i]
+			current_particle_best_fitness = current_particle.best_fitness
+			if current_particle_best_fitness < temporary_best_fitness:
+				temporary_best_position = current_particle.best_position
+				temporary_best_fitness = current_particle.best_fitness
+		self.best_position = temporary_best_position
+		self.best_fitness = temporary_best_fitness
+
+	def update_particles_velocities(self):
+		number_of_particles = len(self.particles)
+		for i in range(number_of_particles):
+			current_particle = self.particles[i]
+			current_particle.update_velocity(self.best_position);
 
 	def update_particles_positions(self):
 		number_of_particles = len(self.particles)
