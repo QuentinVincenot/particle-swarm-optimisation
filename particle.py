2839a843bd53cc92f912f7a8d7db7d4bb0e4be52
Quentin Vincenot <quentin.vincenot@gmail.com>
2019-03-13 20:25:02 +0100

    Implemented very simple particle class with print method

---
+++ particle.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Particle:
+	def __init__(self):
+		self.position = np.random.randint(1000, size=(3, 1))
+		self.velocity = np.random.randint(-50, 50, size=(3, 1))
+	
+	def print(self):
+		print(self.position, self.velocity)
--- pso.py
+++ pso.py
@@ -1,7 +1,14 @@
+from particle import *
+
 import numpy as np
+
 
 def generate_random_objective():
 	return np.random.randint(1000, size=(3, 1))
 
+
 objective = generate_random_objective()
 print(objective)
+
+particle = Particle()
+particle.print()
